a7f7da3a051c87fa74d325dc24172f91b3d06d4c
Vishal Nagda <vishalnagda1@gmail.com>
2018-04-11 13:46:37 +0530

    added error handler and fix string conversation issue in remve project script

--- remove_rails_project.sh
+++ remove_rails_project.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
+set -e
 echo -e "Project Name : \c"
 read pname
 pname="${pname// /_}"
-pname="${pname,,}"
+# pname="${pname,,}"
+pname=$(echo $pname | tr '[:upper:]' '[:lower:]')
 rvm gemset delete $pname # to remove gemset, the gemset name should be the same as project name or you can modify this command.
 rm -rf $pname
 echo "Successfully removed $pname gemset."
